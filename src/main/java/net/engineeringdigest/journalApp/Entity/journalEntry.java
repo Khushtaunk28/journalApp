fdb0fc890619559d7300268008fde5f4024253d7
Khushtaunk28 <Khush.taunk@gmail.com>
2024-08-27 00:18:09 +0530

    ÃÃadding getputpost apis

---
+++ src/main/java/net/engineeringdigest/journalApp/Entity/journalEntry.java
@@ -0,0 +1,32 @@
+package net.engineeringdigest.journalApp.Entity;
+
+public class journalEntry {
+    private long id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    private String title;
+    private String content;
+}
---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,38 @@
+package net.engineeringdigest.journalApp.controller;
+
+import net.engineeringdigest.journalApp.Entity.journalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private HashMap<Long, journalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public ArrayList<journalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+
+    }
+    @PostMapping
+    public boolean createEntry(@RequestBody journalEntry entry) {
+    journalEntries.put(entry.getId(), entry);
+    return true;
+
+    }
+    @GetMapping("/id/{myId} ")
+    public journalEntry getJournalEntryById(@PathVariable long myId) {
+        return journalEntries.get(myId);
+    }
+    @DeleteMapping("id/{myId")
+    public journalEntry deleteEntryById(@PathVariable long myId) {
+        return journalEntries.remove(myId);
+
+    }
+    @PutMapping("id/{id}")
+    public journalEntry updateEntryById(@PathVariable  Long id, @RequestBody journalEntry entry) {
+        return journalEntries.put(id, entry);
+    }
+}
