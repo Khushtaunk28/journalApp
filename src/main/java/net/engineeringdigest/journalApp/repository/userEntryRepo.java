fead0a317af24ab92e8d239d920cfde600c373e6
Khushtaunk28 <Khush.taunk@gmail.com>
2024-09-15 00:00:06 +0530

    added .yml and junit testing

--- src/main/java/net/engineeringdigest/journalApp/repository/userEntryRepo.java
+++ src/main/java/net/engineeringdigest/journalApp/repository/userEntryRepo.java
@@ -3,6 +3,9 @@ package net.engineeringdigest.journalApp.repository;
 import net.engineeringdigest.journalApp.Entity.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 
 public interface userEntryRepo extends MongoRepository<User, ObjectId> {
     User findByusername(String username);
