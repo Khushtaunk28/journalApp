f4c306e0693dd799ce052ed4920c7608e9954711
Khushtaunk28 <Khush.taunk@gmail.com>
2024-09-18 09:52:22 +0530

    show profiles in cli

--- src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
+++ src/main/java/net/engineeringdigest/journalApp/JournalApplication.java
@@ -3,7 +3,9 @@ package net.engineeringdigest.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -14,10 +16,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JournalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JournalApplication.class, args);
-    }
+        ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
+        ConfigurableEnvironment env = context.getEnvironment();
+        System.out.println(env.getActiveProfiles()[0]);
 //    @Bean
 //    public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
 //        return new MongoTransactionManager(dbFactory);
 //    }
+    }
 }
