6d42b92c5d6fe92ec9a27d879c258887892617b9
Khushtaunk28 <Khush.taunk@gmail.com>
2024-12-28 17:45:40 +0530

    added fetch call for signup

--- ReactFrontEnd/reactFrontEnd/src/components/signup.jsx
+++ ReactFrontEnd/reactFrontEnd/src/components/signup.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 const Signup = () => {
+  const [username,setusername]=useState("");
+  const [password,setpassword]=useState("");
+  const handleClick=(e)=>{
+    e.preventDefault()
+    const user={username,password}
+    console.log(user)
+    fetch("http://localhost:8080/user/sign-up",{
+      method: "POST",
+      headers :{"Content-Type":"application/json"},
+      body:JSON.stringify(user)
+    }).then(()=>{
+      console.log("New user signed up")
+    })
+  }
   return (
     <div className='signup-form'>
         <main>
@@ -7,12 +22,17 @@ const Signup = () => {
         <form action="sign-up" className='formcss'>
           <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" name="username" id="username" placeholder='Enter your name' />
+          <input type="text" name="username" id="username" placeholder='Enter your name' 
+          value={username}
+          onChange={(e)=>setusername(e.target.value)} />
           </div>
-          <div><label htmlFor="pass">Password:</label>
-          <input type="password" placeholder='********' />
+          <div><label htmlFor="pass">Password:</label >
+          <input type="password" placeholder='********'
+          value={password}
+          onChange={(e)=>setpassword(e.target.value)} />
           </div>
-            <button type='Submit'>Sign-Up</button>
+            <button type='Submit'
+            onClick={handleClick}>Sign-Up</button>
         </form>
         </main>
     </div>
